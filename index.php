9834dd5cde14e7568e8f4107d689b2e363ab179d
YAO <yaobozi@gmx.fr>
2024-01-17 23:15:02 +0100

    Folio

--- index.php
+++ index.php
@@ -39,17 +39,7 @@
 
 			<!-- Main Navigation -->
 			<nav class="main_nav_container">
-				<div class="main_nav">
-					<ul class="main_nav_list">
-						<li class="main_nav_item"><a href="index.php">Accueil</a></li>
-						<li class="main_nav_item"><a href="#">A propos</a></li>
-						<li class="main_nav_item"><a href="formations.php">Formations</a></li>
-						<li class="main_nav_item"><a href="elements.html">Services</a></li>
-						<!--<li class="main_nav_item"><a href="news.html">Livres</a></li>-->
-						<li class="main_nav_item"><a href="livres.php">Livres</a></li>
-						<li class="main_nav_item"><a href="contact.php">Contact</a></li>
-					</ul>
-				</div>
+				<?php include("navbar.php"); ?> 
 			</nav>
 		</div>
 		<div class="header_side d-flex flex-row justify-content-center align-items-center">
@@ -159,7 +149,7 @@
 							<img src="images/earth-globe.svg" class="svg" alt="">
 							<div class="hero_box_content">
 								<h2 class="hero_box_title">Formations</h2>
-								<a href="courses.html" class="hero_box_link">Plus de détails</a>
+								<a href="formations.php" class="hero_box_link">Plus de détails</a>
 							</div>
 						</div>
 					</div>
@@ -169,7 +159,7 @@
 							<img src="images/books.svg" class="svg" alt="">
 							<div class="hero_box_content">
 								<h2 class="hero_box_title">Ma librairie</h2>
-								<a href="courses.html" class="hero_box_link">Decouvrons la</a>
+								<a href="tousLivres.php" class="hero_box_link">Decouvrons la</a>
 							</div>
 						</div>
 					</div>
@@ -178,8 +168,8 @@
 						<div class="hero_box d-flex flex-row align-items-center justify-content-start">
 							<img src="images/professor.svg" class="svg" alt="">
 							<div class="hero_box_content">
-								<h2 class="hero_box_title">Our Teachers</h2>
-								<a href="teachers.html" class="hero_box_link">view more</a>
+								<h2 class="hero_box_title">Autres</h2>
+								<a href="teachers.html" class="hero_box_link">Détails</a>
 							</div>
 						</div>
 					</div>
@@ -562,134 +552,7 @@
 	<!-- Footer -->
 
 	<footer class="footer">
-		<div class="container">
-			
-			<!-- Newsletter -->
-
-			<div class="newsletter">
-				<div class="row">
-					<div class="col">
-						<div class="section_title text-center">
-							<h1>Subscribe to newsletter</h1>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="col text-center">
-						<div class="newsletter_form_container mx-auto">
-							<form action="post">
-								<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-center">
-									<input id="newsletter_email" class="newsletter_email" type="email" placeholder="Email Address" required="required" data-error="Valid email is required.">
-									<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">Subscribe</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-
-			</div>
-
-			<!-- Footer Content -->
-
-			<div class="footer_content">
-				<div class="row">
-
-					<!-- Footer Column - About -->
-					<div class="col-lg-3 footer_col">
-
-						<!-- Logo -->
-						<div class="logo_container">
-							<div class="logo">
-								<img src="images/logo.png" alt="">
-								<span>course</span>
-							</div>
-						</div>
-
-						<p class="footer_about_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum, tempor lacus.</p>
-
-					</div>
-
-					<!-- Footer Column - Menu -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Menu</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_list_item"><a href="#">Accueil</a></li>
-								<li class="footer_list_item"><a href="#">A propos</a></li>
-								<li class="footer_list_item"><a href="courses.html">Formations</a></li>
-								<li class="footer_list_item"><a href="news.html">Services</a></li>
-								<li class="footer_list_item"><a href="contact.html">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<!-- Footer Column - Usefull Links -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Usefull Links</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_list_item"><a href="#">Testimonials</a></li>
-								<li class="footer_list_item"><a href="#">FAQ</a></li>
-								<li class="footer_list_item"><a href="#">Community</a></li>
-								<li class="footer_list_item"><a href="#">Campus Pictures</a></li>
-								<li class="footer_list_item"><a href="#">Tuitions</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<!-- Footer Column - Contact -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Contact</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/placeholder.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>
-									Blvd Libertad, 34 m05200 Arévalo
-								</li>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/smartphone.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>
-									0034 37483 2445 322
-								</li>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/envelope.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>hello@company.com
-								</li>
-							</ul>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-			<!-- Footer Copyright -->
-
-			<div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
-				<div class="footer_copyright">
-					<span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
-				</div>
-				<div class="footer_social ml-sm-auto">
-					<ul class="menu_social">
-						<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-			</div>
-
-		</div>
+		<?php include("piedPage.php"); ?>
 	</footer>
 
 </div>
---
+++ navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+?>
+				<div class="main_nav">
+					<ul class="main_nav_list">
+						<li class="main_nav_item"><a href="index.php">Accueil</a></li>
+						<li class="main_nav_item"><a href="#">A propos</a></li>
+						<li class="main_nav_item"><a href="formations.php">Formations</a></li>
+						<li class="main_nav_item"><a href="elements.html">Services</a></li>
+						<!--<li class="main_nav_item"><a href="news.html">Livres</a></li>-->
+						<li class="main_nav_item"><a href="livres.php">Livres</a></li>
+						<li class="main_nav_item"><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
